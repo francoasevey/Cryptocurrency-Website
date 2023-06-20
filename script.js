e5314e511ea3e055be112f2cf32fbb2f89b36579
francoromeo <francoromeo5012@gmail.com>
2023-06-20 13:09:01 -0300

    convertion js

--- script.js
+++ script.js
@@ -31,3 +31,43 @@ const date = () =>{
 }
 //date();
 setInterval(date, 1000);
+
+let coin_first = document.getElementById('coin_first');
+let coin = document.getElementById('coin');
+let btc = document.getElementById('btc');
+
+coin.addEventListener('change', () => {
+    let a = coin.value;
+    coin_first.innerText = a.slice(0, 1);
+    if (a === "Bitcoin"){
+        btc.innerText = "BTC";
+    } else if (a === "Ethereum") {
+        btc.innerText = "ETH";
+    } else if (a === "Tether") {
+        btc.innerText = "USDT";
+    } else {
+        btc.innerText = "BNB"
+    }
+})
+
+let gov_coins = document.getElementById('gov_coins');
+let coins = document.getElementById('coins');
+let gov_coinss = document.getElementById('gov_coinss');
+
+coins.addEventListener('change', () => {
+    let a = coins.value;
+    gov_coins.innerText = a.slice(0, 1);
+    if (a === "Dollar"){
+        gov_coins.innerText = "$";
+        gov_coinss.innerText = "USD";
+    } else if (a === "Ruppe") {
+        gov_coinss.innerText = "INR";
+        gov_coins.innerText = "₹";
+    } else if (a === "Riyal") {
+        gov_coins.innerText = "S";
+        gov_coinss.innerText = "SAR";
+    } else {
+        gov_coins.innerText = "A"
+        gov_coinss.innerText = "AED";
+    }
+})
