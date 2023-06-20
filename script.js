88fa0c962d991366c2f3ffb645eb026b959769f8
francoromeo <francoromeo5012@gmail.com>
2023-06-20 12:32:04 -0300

    count js¿

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const date = () =>{
-    let date_main = new Date("Nov, 04, 2022, 10:50:58");
+    let date_main = new Date("Nov, 04, 2023, 10:50:58");
 
     let current_date = new Date();
 
@@ -7,8 +7,27 @@ const date = () =>{
 
     let date_hou = parseInt((date_main - current_date) / 1000 / 60 / 60) % 24;
 
-    let date_min = parseInt((date_main - current_date) / 1000 / 60) % 24;
+    let date_min = parseInt((date_main - current_date) / 1000 / 60) % 60;
 
+    let date_sec = parseInt((date_main - current_date) / 1000) % 60;
 
+    document.getElementsByClassName('time')[0].innerHTML = `
+            <span>${date_day}<br>
+            <h6>DAYS</h6>
+            </span>
+            <h5>:</h5>
+            <span>${date_hou}<br>
+            <h6>HOURS</h6>
+            </span>
+            <h5>:</h5>
+            <span>${date_min}<br>
+            <h6>MINUTES</h6>
+            </span>
+            <h5>:</h5>
+            <span>${date_sec}<br>
+            <h6>SECONDS</h6>
+            </span>
+            `
 }
-date();
+//date();
+setInterval(date, 1000);
