240d40bcc9082382f6c6b183592fc8092697e624
francoromeo <francoromeo5012@gmail.com>
2023-06-19 23:57:23 -0300

    js 3

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const date = () =>{
 
     let current_date = new Date();
 
-    let date_day = parseInt((date_main - current_date) / 1000 / 60 / 60)
+    let date_day = parseInt((date_main - current_date) / 1000 / 60 / 60 / 24);
 
-    let date_you =
+    let date_hou = parseInt((date_main - current_date) / 1000 / 60 / 60) % 24;
+
+    let date_min = parseInt((date_main - current_date) / 1000 / 60) % 24;
+
+
 }
 date();
