895126858046fc93eaf7ddf859cb48b7d5b83308
francoromeo <francoromeo5012@gmail.com>
2023-06-19 23:54:35 -0300

    js 2

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const date = () =>{
     let date_main = new Date("Nov, 04, 2022, 10:50:58");
+
+    let current_date = new Date();
+
+    let date_day = parseInt((date_main - current_date) / 1000 / 60 / 60)
+
+    let date_you =
 }
+date();
